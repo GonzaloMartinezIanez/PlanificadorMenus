96c5b2710250047a921803675e7de6c331befd67
GonzaloMartinezIanez <gonzalomartianez@gmail.com>
2026-07-27 20:36:42 +0200

    Endpoints: Filtrar ingredientes por nombre y GET de todos los ingredientes de una categoría

--- pmdjango/ingredients/urls.py
+++ pmdjango/ingredients/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import IngredientCategoryViewSet, IngredientViewSet
+from .views import IngredientByNameApiView, IngredientCategoryViewSet, IngredientViewSet, IngredientCategoryIngredientsApiView
 from rest_framework.routers import DefaultRouter
 
 ingredient_router = DefaultRouter()
@@ -9,7 +9,9 @@ ingredient_category_router = DefaultRouter()
 ingredient_category_router.register(r'', IngredientCategoryViewSet, basename='ingredient-category-viewset')
 
 urlpatterns = [
+    path('ingredient_by_name/', IngredientByNameApiView.as_view(), name='ingredient-by-name-apiview'),
     path('ingredients/', include(ingredient_router.urls)),
     path('ingredient_categories/', include(ingredient_category_router.urls)),
+    path('ingredient_categories/<int:id_ingredient_category>/ingredients/', IngredientCategoryIngredientsApiView.as_view(), name='ingredient-category-ingredients-apiview'),
 ]
 
--- pmdjango/ingredients/views.py
+++ pmdjango/ingredients/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
 from .models import IngredientCategory, Ingredient
 from .serializers import IngredientCategorySerializer, IngredientSerializer
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+# CRUD de Categorías de ingredientes
 class IngredientCategoryViewSet(viewsets.ModelViewSet):
   permission_classes = []
   queryset = IngredientCategory.objects.all()
@@ -15,14 +18,42 @@ class IngredientCategoryViewSet(viewsets.ModelViewSet):
         kwargs['many'] = True
     return super().get_serializer(*args, **kwargs)
 
+# Endpoint que devuelve los ingredientes de una categoría. Las categorías principales no devuelven nada
+class IngredientCategoryIngredientsApiView(APIView):
+  permission_classes = []
+
+  def get(self, request, id_ingredient_category):
+    if not IngredientCategory.objects.filter(id_ingredient_category = id_ingredient_category).exists():
+      return Response({"error": "La categoria no existe."}, status = status.HTTP_404_NOT_FOUND)
+
+    ingredients = Ingredient.objects.filter(id_ingredient_categories__id_ingredient_category = id_ingredient_category)
+    serializer = IngredientSerializer(ingredients, many = True)
+
+    return Response(serializer.data, status = status.HTTP_200_OK)
+
 class IngredientViewSet(viewsets.ModelViewSet):
   permission_classes = []
   queryset = Ingredient.objects.all()
   serializer_class = IngredientSerializer
   lookup_field = 'id_ingredient'
+  lookup_value_regex = '[^/]+'
 
   # Permitir insertar múltiples ingredientes en la misma petición
   def get_serializer(self, *args, **kwargs):
     if isinstance(kwargs.get('data', {}), list):
         kwargs['many'] = True
     return super().get_serializer(*args, **kwargs)
+
+# Endpoint para buscar un ingrediente por texto
+class IngredientByNameApiView(APIView):
+  permission_classes = []
+
+  def get(self, request):
+    name = request.GET.get('name', '') # Capta el query param de la url /ingredient_by_name/?name=Jamon
+    if not name:
+      return Response({"error": "El parámetro 'name' es obligatorio."}, status = status.HTTP_400_BAD_REQUEST)
+
+    ingredients = Ingredient.objects.filter(name__icontains = name) # Con icontains se ignoran mayúsculas y minúsculas
+    serializer = IngredientSerializer(ingredients, many = True)
+
+    return Response(serializer.data, status = status.HTTP_200_OK)
